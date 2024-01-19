4c20e5f11018f1b8e8fd69d0a57e34bc8e5cf5f4
AlexDv <DvoinikovAV@yandex.ru>
2024-01-19 10:29:01 +0300

    Домашняя работа 4 задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
+        //Задание 1
         byte age=20;
         if (age<18)
         {
@@ -9,6 +10,17 @@ public class Main {
         else
         {
             System.out.println("Вы совершеннолетний.");
+        }
+        //Задание 2
+        byte temp=4;
+        System.out.println("Сегодня "+temp+" градусов.");
+        if (temp<5)
+        {
+            System.out.println("Холодно, нужно надеть шапку");
+        }
+        else
+        {
+            System.out.println("Тепло, можно идти без шапки");
         }
 
     }

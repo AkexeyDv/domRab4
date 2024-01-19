2487c100cfc8ef6ac333f925d147d3512c14e3f2
AlexDv <DvoinikovAV@yandex.ru>
2024-01-19 10:19:57 +0300

    Домашняя работа 4 задание 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        byte age=20;
+        if (age<18)
+        {
+            System.out.println("Вы не достигли совепшеннолетия. Нужно немного подождать.");
+        }
+        else
+        {
+            System.out.println("Вы совершеннолетний.");
+        }
+
+    }
+}
